c48a3aacabf7e75524df0be295b2fe182ce8ac3a
Josephscha <Josephshincha@gmail.com>
2020-02-24 17:54:29 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
 
+class Student < User
+    attr_accessor :knowledge
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge_input)
+        #takes in arguement of string/knowledge and adds to student's knowledge array.
+        @knowledge << knowledge_input
+    end
+
+    def knowledge
+        #returns students knowledge array
+        @knowledge
+    end
 end

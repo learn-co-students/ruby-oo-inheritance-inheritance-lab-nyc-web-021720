9edb72d3d4cf0e0db3ddead70bf38e8a34ae7a23
JohnsonNYC <jkow95@gmail.com>
2020-02-26 16:20:13 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User
+    attr_accessor :knowledge
+    @@all = []
+    def initialize
+        super(first_name,last_name)
+        Student.all << self
+        @knowledge = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def learn(string)
+        knowledge << string
+    end
 end

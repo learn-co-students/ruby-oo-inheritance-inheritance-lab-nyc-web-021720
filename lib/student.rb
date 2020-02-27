535b48ccdcd830bf69937f02d78b3c54607779f1
Lix311 <alex.sterrantino@gmail.com>
2020-02-26 21:31:45 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge 
+    def initialize
+        @knowledge = []
+    end 
+
+    def learn(string)
+        knowledge.push(string)
+    end 
+
+    def knowledge 
+        @knowledge 
+    end 
 
 end
